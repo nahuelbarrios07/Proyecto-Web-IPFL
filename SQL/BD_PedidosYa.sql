2e7f150f562ad8f494916402d9b7399ba407187b
nahuelbarrios07 <111724138+nahuelbarrios07@users.noreply.github.com>
2022-11-13 16:19:02 -0300

    Agrego la Base de Datos con las tablas creadas

---
+++ SQL/BD_PedidosYa.sql
@@ -0,0 +1,43 @@
+-- Creación de la Base de datos Pedidos Ya
+
+CREATE DATABASE "Pedidos Ya";
+
+--Creación de la tabla usuario de la Base de Datos Pedidos Ya.
+
+CREATE TABLE "usuario"(
+    id_usuario VARCHAR (20) PRIMARY KEY,
+    nombre_usuario VARCHAR (100) NOT NULL,
+    contraseña VARCHAR (30),
+    direccion VARCHAR (100)
+)
+
+--Creación de la tabla producto de la Base de Datos Pedidos Ya.
+
+CREATE TABLE "producto"(
+    id_producto VARCHAR(20) PRIMARY KEY,
+    descripcion VARCHAR (200),
+    proveedor VARCHAR (150)
+)
+
+--Creación de la tabla pedido de la Base de Datos Pedidos Ya.
+
+CREATE TABLE "pedido"(
+    producto VARCHAR (100),
+    usuario VARCHAR (50) NOT NULL,
+    cantidad INT,
+    id_factura VARCHAR (50),
+    monto FLOAT,
+    FOREIGN KEY (id_factura) REFERENCES factura (id_factura)
+)
+
+--Creación de la tabla factura de la Base de Datos Pedidos Ya.
+
+CREATE TABLE "factura"(
+    id_factura VARCHAR (50) PRIMARY KEY,
+    monto FLOAT,
+    tipo_pago VARCHAR (50)
+)
+
+
+
+
