aee0c0639199c843e6c3c5135a4d5f9b3b6cc7b5
nahuelbarrios07 <nahuelbarrios@yahoo.com.ar>
2022-11-24 01:35:08 -0300

    Modifique el JS del login y corregí en el HTML

--- WEB/Login.js
+++ WEB/Login.js
@@ -1,13 +1,14 @@
-function validate()
-{
-var username=document.getElementById("usuario").value;
-var password=document.getElementById("contrasena").value;
-if(username=="usuario_01"&& password=="12345678")
-{
-    window.location= "D:\Documentos\Documentos\Clases\Programacion_web\Proyecto\Proyecto-web-IPFL\WEB\pedidos-usuario.html";
-}
-else
-{
-    alert("Usuario o Contraseña incorrecto");
-}
+//Comprobar si el usuario y la contraseña ingresados en el HTML son correctos.
+
+function validate(){
+
+    let username=document.getElementById("usuario").value;
+    let password=document.getElementById("contrasena").value;
+
+    if(username=="usuario_01"&& password=="12345678"){
+        window.location= "pedidos-usuario.html";
+        }else{     
+        alert("Usuario o Contraseña incorrecto");
+        }
+        
 }
